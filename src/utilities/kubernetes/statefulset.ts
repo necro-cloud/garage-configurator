35ddedf163a4bd17e037928414abda7704a23ab4
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2025-07-28 15:42:18 +0530

    feat(kubernetes operations): API implementations for running operations on the garage statefulset

---
+++ src/utilities/kubernetes/statefulset.ts
@@ -0,0 +1,21 @@
+import k8s from "@kubernetes/client-node";
+
+// Method to check StatefulSet if the number
+// of ready replicas is the same as the desired amount
+export async function checkStatefulSetClusterStatus(
+	statefulSetApi: k8s.AppsV1Api,
+	garageKubernetesClusterName: string,
+	garageKubernetesNameSpace: string,
+	garageKubernetesDesiredReplicas: number,
+) {
+	const response = await statefulSetApi.readNamespacedStatefulSet({
+		name: garageKubernetesClusterName,
+		namespace: garageKubernetesNameSpace,
+	});
+
+	if (response.status?.readyReplicas === garageKubernetesDesiredReplicas) {
+		return true;
+	} else {
+		return false;
+	}
+}
